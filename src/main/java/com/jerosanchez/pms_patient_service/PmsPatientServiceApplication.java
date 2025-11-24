d38e14a74f0786f217ef2c378ab6aa4d3543076e
Jero Sanchez <dev@jerosanchez.com>
2025-11-24 14:08:52 +0100

    Create service project

---
+++ src/main/java/com/jerosanchez/pms_patient_service/PmsPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jerosanchez.pms_patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerosanchez/pms_patient_service/PmsPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerosanchez.pms_patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
